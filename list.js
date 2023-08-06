4f4ed5776dcd2d3e8af561e639d1ceb50ea69148
Fangwei Jiang <emberspirit2018@gmail.com>
2023-08-06 17:37:49 +1000

    final code

--- list.js
+++ list.js
@@ -1,4 +1,23 @@
-let user_list = document.getElementById("user_list");
+const user_list = document.getElementById("user_list");
+
+function judgeContinue() {
+    let judge = prompt(`Are you sure you want to delete all users? Y/N`);
+    if (judge === null || !(judge.toLowerCase() === "y")) return false;
+    return true;
+}
+
+async function deleteSingle() {
+    try {
+        if (!judgeContinue()) return;
+        const response = await axios.delete(`http://localhost:8080/api/clearsingle?id=${this.getAttribute("data-id")}`);
+        if (response.status === 201) {
+            alert("Delete successfully");
+            getUserList();
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 async function getUserList() {
     try {
@@ -15,38 +34,19 @@ async function getUserList() {
             li.innerHTML = `
             <span>${value.name}</span>
             <span>${value.age}</span>
-            <button class="delete_btn">Delete</button>`;
+            <button class="delete_btn" data-id="${value.id}">Delete</button>`;
             user_list.appendChild(li);
         }
-        user_list.querySelectorAll(".delete_btn").forEach(item =>
-            item.addEventListener("click", async e => {
-                const name = e.target.closest("li").querySelector("span").textContent;
-                console.log(name);
-                try {
-                    let judge = prompt(`Are you sure you want to delete? Y/N`);
-                    if (!(judge.toLowerCase() === "y")) return;
-                    const response = await axios.delete(`http://localhost:8080/api/clearsingle?name=${name}`);
-                    if (response.status === 201) {
-                        alert("Delete successfully");
-                        getUserList();
-                    }
-                } catch (err) {
-                    console.error(err);
-                }
-            })
-        );
+        user_list.querySelectorAll(".delete_btn").forEach(item => item.addEventListener("click", deleteSingle));
     } catch (error) {
         console.error("Error fetching users:", error);
     }
 }
 
-document.getElementById("get_list").addEventListener("click", getUserList);
-
-document.getElementById("add_user").addEventListener("click", async () => {
+async function addUser() {
     try {
         if (document.getElementById("name").value === "" || document.getElementById("age").value === "") {
             alert("please fill in all fields");
-            getUserList();
             return;
         }
         const body = {
@@ -57,21 +57,17 @@ document.getElementById("add_user").addEventListener("click", async () => {
         const users = response.data.data;
         alert("user: " + users.name + " age: " + users.age + " has been added");
         getUserList();
-        // const p = document.createElement("p");
-        // p.textContent = "user: " + users.name + " age: " + users.age + " has been added";
-        // user_list.appendChild(p);
         console.log("users", users);
         document.getElementById("name").value = "";
         document.getElementById("age").value = "";
     } catch (error) {
         console.error("Error fetching users:", error);
     }
-});
+}
 
-document.getElementById("clear_list").addEventListener("click", async () => {
+async function clear_list() {
     try {
-        let judge = prompt(`Are you sure you want to delete all users? Y/N`);
-        if (!(judge.toLowerCase() === "y")) return;
+        if (!judgeContinue()) return;
         const response = await axios.delete("http://localhost:8080/api/clear");
         const user = response.data;
         alert(user);
@@ -79,6 +75,9 @@ document.getElementById("clear_list").addEventListener("click", async () => {
     } catch (error) {
         console.error("Error fetching users:", error);
     }
-});
+}
 
+document.getElementById("add_user").addEventListener("click", addUser);
+document.getElementById("get_list").addEventListener("click", getUserList);
+document.getElementById("clear_list").addEventListener("click", clear_list);
 getUserList();
--- router.js
+++ router.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const router = express.Router();
+
 let user = [
-    { name: "Ben", age: 20 },
-    { name: "Amy", age: 23 },
-    { name: "Chris", age: 34 },
-    { name: "Jack", age: 18 },
+    { id: 1, name: "Ben", age: 20 },
+    { id: 2, name: "John", age: 30 },
+    { id: 3, name: "Chris", age: 40 },
+    { id: 4, name: "Jane", age: 60 },
 ];
 
-router.get("/api/users", (req, res) => res.send(user));
+router.get("/api/users", (req, res) => res.status(200).json(user));
 router.post("/api/user", (req, res) => {
     user.push(req.body);
-    res.send({
+    res.status(201).json({
         msg: "add user successfully",
         data: req.body,
     });
 });
 
 router.delete("/api/clearsingle", (req, res) => {
-    const messageName = req.query.name;
-    const messageIndex = user.findIndex(msg => msg.name === messageName);
+    const messageID = parseInt(req.query.id);
+    const messageIndex = user.findIndex(user => user.id === messageID);
     if (messageIndex !== -1) {
+        if (messageIndex !== user.length - 1) for (let i = messageIndex + 1; i < user.length; i++) --user[i].id;
         const deletedMessage = user.splice(messageIndex, 1)[0];
         console.log("deletedMessage", deletedMessage);
         res.status(201).json({ msg: "delete successfully", deleteData: deletedMessage });
@@ -28,7 +30,7 @@ router.delete("/api/clearsingle", (req, res) => {
 
 router.delete("/api/clear", (req, res) => {
     user = [];
-    res.send("User list has been cleared");
+    res.status(201).send("User list has been cleared");
 });
 
 module.exports = router;
